4b7cc95c5ab51c5ff79a05ab449569e2c5ad63c5
Aric <aessig32@hotmail.com>
2018-02-13 16:38:37 -0600

    added  function. Trying to append the list item to the post-it

--- scripts/postIt.js
+++ scripts/postIt.js
@@ -14,18 +14,33 @@ $(document).ready(function(){
 	});
 	});	
 	function addPostIt(value) {
-		
-		// var $label = $('<label class="item"></label>').append('<input type="checkbox">', '<span class="checkbox"></span>');
-		// var $title = $('<h3 class="title"></h3>').text(value);
-		// 			$($label).appendTo($title)
-		// 			.appendTo('<h3 class="title"></h3>');
-		// var $div = $('<div class="note"></div>').append('<button class="remove buttons"></button>', $title)
-		// 			.appendTo("div#main");
-					
-		var $label = $('<label class="item"></label>').append('<input type="checkbox">', '<span class="checkmark"></span>');
+		var $listButton = $('<button class="listButton buttons"></button>');
+		var $input = $('<input class="itemInput" type="text" placeholder="add an activity and press enter">').append($listButton);
+		var $listItem = $($listButton).on("click", function(){
+			addItemText($listButton);
+		});
+		var $label = $('<label class="item"></label>').append('<input type="checkbox">', '<span class="checkmark"></span>')
+						.append($listItem);
+						console.log($listItem);
 		var $title = $('<h3 class="title"></h3>').text(value);
-		var $todo = $('<h3 class="title"></h3>',).append($title, $label);
+		var $todo = $('<h3 class="title"></h3>',).append($title, $input, $listButton, $label,);
 		var $div = $('<div class="note"></div>').append('<button class="remove buttons"></button>', $todo);
 		$("div#main").append($div);
-		console.log($div);
 	};
+	function addItemText($listButton) {
+		var value = $($listButton).siblings("input").val();
+		var text = $($listButton).siblings("label").text(value);
+			return text;
+	}
+
+	
+	// function clickList() {
+		// var input = document.getElementsByClassName("listButton");
+		// console.log(input);
+		// input.addEventListener("keyup", function(event) {
+		// 	if (event.keyCode === 13) {
+		// 	document.getElementsByClassName("itemInput").click();
+		// 	console.log("button clicked");
+		
+	// }
+  	
